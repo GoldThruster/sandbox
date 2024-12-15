a89f8e0c864a4531f4a68e645a9ad0f16993d712
GoldThruster <gold.thruster@gmail.com>
2024-12-15 17:13:25 +0000

    feat(cli): :sparkles: program defaults to 'sh'

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,7 @@
 #[derive(clap::Parser)]
 pub struct Cli {
     /// The name of the program to run
+    #[arg(default_value_t = String::from("sh"))]
     pub program: String,
     /// Arguments for the program to run
     pub args: Vec<String>,
