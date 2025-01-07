089111e48f1157b17ae6105fab17820333bbdf6c
GoldThruster <gold.thruster@gmalil.com>
2025-01-07 01:15:50 +0100

    feat(isolate): :sparkles: use tmp dir if `new_root` not provided

--- src/isolate.rs
+++ src/isolate.rs
@@ -1,4 +1,8 @@
-use std::path::Path;
+use std::{
+    borrow::Cow,
+    env, fs, io,
+    path::{Path, PathBuf},
+};
 use thiserror::Error;
 
 pub mod cli;
@@ -10,7 +14,28 @@ pub enum Error {
     SetRoot(#[from] set_root::Error),
 }
 
-pub fn isolate(new_root: &Path) -> Result<(), Error> {
-    set_root::set_root(new_root)?;
+fn isolate(new_root: Option<&Path>) -> Result<(), Error> {
+    let root_dir = new_root.map_or_else(
+        || Cow::Owned(tmp_root_path().expect("unable to create tmp dir")),
+        Cow::Borrowed,
+    );
+
+    set_root::set_root(root_dir.as_ref())?;
     Ok(())
+}
+
+pub fn tmp_root_path() -> io::Result<PathBuf> {
+    let mut path = env::temp_dir();
+    path.push("sandbox");
+
+    let mut uuid = random_string();
+    uuid.push_str("_box");
+
+    path.push(uuid);
+
+    fs::create_dir_all(&path).map(|_| path)
+}
+
+fn random_string() -> String {
+    rand::random::<usize>().to_string()
 }
--- src/isolate/cli.rs
+++ src/isolate/cli.rs
@@ -2,11 +2,11 @@ use std::path::PathBuf;
 
 #[derive(clap::Args)]
 pub struct Args {
-    /// The directory to use as root
+    /// The directory to use as root [default: /tmp/<new-dir>]
     #[arg(short, long)]
-    pub root: PathBuf,
+    pub root: Option<PathBuf>,
 }
 
 pub fn isolate_with_args(args: Args) -> Result<(), super::Error> {
-    super::isolate(&args.root)
+    super::isolate(args.root.as_deref())
 }
